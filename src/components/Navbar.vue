7d771354713d56f05357d13afed9e13360c9f67a
Oleh Didyk <oleggsa@gmail.com>
2022-01-31 12:12:45 +0200

    added router link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,29 @@
 <template>
     <div class="navbar">
-        <el-button @click="$router.push('/')" type="danger">Main</el-button>
-        <el-button @click="$router.push('/list')" type="danger">Users list</el-button>
-        <el-button @click="$router.push('/about')" type="danger">About</el-button>
+        <el-button type="danger" v-for="link in this.navbarLinks" :key="link.name">
+            <router-link :to="link[0]">{{link[1]}}</router-link>
+        </el-button>
+        <!-- <el-button type="danger" @click="console">
+            <router-link to="/list">Users</router-link>
+        </el-button>
+        <el-button type="danger">
+            <router-link to="/about">About</router-link>
+        </el-button> -->
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: 'Navbar',
+        data(){
+            return {
+                navbarLinks: [
+                    ['/', 'Main'], 
+                    ['/list', 'Users'], 
+                    ['/about', 'About']
+                ]
+            }
+        }
     }
 </script>
 
@@ -22,5 +37,9 @@
         justify-content: center;
         margin-bottom: 20px;
         box-sizing: border-box;
+    }
+    a {
+        text-decoration: none;
+        color: white;
     }
 </style>
