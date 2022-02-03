5dc23ecf10863c2f88bf3ffa1774257d927d9ba9
Oleh Didyk <oleggsa@gmail.com>
2022-02-03 14:06:39 +0200

    Single user request

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,10 +2,11 @@ import ApiClient from "../libs/api-client";
 
 const baseUrl = process.env.VUE_APP_USERS_API
 
-console.log(baseUrl);
-
 export default {
     usersList(perPage, skip) {
         return ApiClient.get(`${baseUrl}/users?perPage=${perPage}&skip=${skip}`)
+    },
+    userData(userId) {
+        return ApiClient.get(`${baseUrl}/users/${userId}`)
     }
 }
